6f5387a11f9f33f2c31dda01933d19364b16e386
SeerjUeo <sergiu.gata@emanuel.ro>
2020-12-17 20:27:56 +0200

    converted classes to functions

---
+++ src/Game.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Board from "./Board";
+
+const Game = () => {
+    return (
+      <div>
+        <h1>Rock, Paper, Scissors</h1>
+        <Board />
+      </div>
+    );
+}
+
+export default Game;
---
+++ src/Square.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Square = ({ onClick, value = [] }) => {
+
+    return (
+      <div className="square" onClick={onClick}>
+        {value}
+      </div>
+    );
+  }
+
+export default Square;
